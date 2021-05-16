ffa450fe456b69a286d79e56f04caae094e5aaef
Teddy <livistteddy@gmail.com>
2021-05-16 12:13:20 +0100

    Create Solution

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function camelCase(str) {
   strArray = str.split('')
+  wordCounter = 0
+  for (let i in strArray) {
+    if (strArray[i] == strArray[i].toUpperCase()) {
+      wordCounter += 1
+    }
+  }
+  console.log(wordCounter)
 }
+
+camelCase("someString")
