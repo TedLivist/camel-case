9747f50e53c69280bd723476ba6a01c523f220a6
Teddy <livistteddy@gmail.com>
2021-05-16 12:22:00 +0100

    Correctly declare wordCounter variable

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function camelCase(str) {
   let strArray = str.split('')
-  wordCounter = 0
+  let wordCounter = 0
   for (let i in strArray) {
     if (strArray[i] == strArray[i].toUpperCase()) {
       wordCounter += 1
